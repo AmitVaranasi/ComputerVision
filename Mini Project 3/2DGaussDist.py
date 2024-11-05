321de0ddecce251a864536b933de4db9d325d813
Amit Maheshwar Varanasi <amitmaheshwarvaranasi@Amits-MacBook-Pro.local>
2024-11-05 17:25:39 -0600

    updated the code for the 2d gaussian segmentation

--- Mini Project 3/2DGaussDist.py
+++ Mini Project 3/2DGaussDist.py
@@ -4,7 +4,7 @@ import math
 import matplotlib.pyplot as plt
 
 # Define the multivariate Gaussian PDF function
-def multivariate_gaussian_pdf(x, mean, cov, det_sigma, inv_sigma):
+def multivariate_gaussian_pdf(x, mean, det_sigma, inv_sigma):
     diff = x - mean
     exponent = -0.5 * np.dot(np.dot(diff.T, inv_sigma), diff)
     norm_factor = 1.0 / (2 * np.pi * np.sqrt(det_sigma))
@@ -29,39 +29,48 @@ def calculate_hs_histogram(img):
     return cov, mean
 
 # Training
+hs_values_collection = []
 sum_cov = np.zeros((2, 2))
 sum_mean = np.zeros((2,))
 for i in range(1, 13):
-    print(i)
     img_train = cv2.imread(f'/Users/amitmaheshwarvaranasi/Desktop/{i}.png')
-    cov, mean = calculate_hs_histogram(img_train)
-    sum_cov += cov
-    sum_mean += mean
-sum_cov = sum_cov / 12
-sum_mean = sum_mean / 12
-sum_mean = sum_mean.reshape(-1, 1)  # Reshape mean to (2, 1)
+    #cov, mean = calculate_hs_histogram(img_train)
+    hsv_image = cv2.cvtColor(img_train, cv2.COLOR_BGR2HSV)
+    train_hue, train_saturation = hsv_image[:, :, 0], hsv_image[:, :, 1]
+    hs_values = np.column_stack((train_hue.flatten(),train_saturation.flatten()))
+    hs_values_collection.append(hs_values)
 
-det_sigma = np.linalg.det(sum_cov)
-inv_sigma = np.linalg.inv(sum_cov)
+hs_values_collection = np.vstack(hs_values_collection)
+training_datamean = np.mean(hs_values_collection,axis=0)
+trainingdata_covarience = np.zeros((2, 2))
+
+for i in range(len(hs_values_collection)):
+    diff = hs_values_collection[i] - training_datamean
+    diff_reshape = diff.reshape(-1, 1)
+    trainingdata_covarience += np.dot(diff_reshape, diff_reshape.T)
+
+trainingdata_covarience/=len(hs_values_collection)-1
+
+det_sigma = np.linalg.det(trainingdata_covarience)
+inv_sigma = np.linalg.inv(trainingdata_covarience)
 
 # Testing
-test_image = cv2.imread('testing_image.bmp')
+test_image = cv2.imread('./testing_image.bmp')
 hsv_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2HSV)
 hue, saturation = hsv_image[:, :, 0], hsv_image[:, :, 1]
 pixels = np.column_stack((hue.flatten(), saturation.flatten()))
 
 probabilities = np.array([
-    multivariate_gaussian_pdf(pixel, sum_mean.flatten(), sum_cov, det_sigma, inv_sigma)
+    multivariate_gaussian_pdf(pixel, training_datamean, det_sigma, inv_sigma)
     for pixel in pixels
 ])
 probability_map = probabilities.reshape(hue.shape)
 
 # Thresholding and segmentation
-threshold = 0.0001  # Adjust the threshold as needed
+threshold = 0.0001 # Adjust the threshold as needed
 skin_mask = probability_map > threshold
 skin_segmented_image = np.zeros_like(test_image)
 skin_segmented_image[skin_mask] = test_image[skin_mask]
-
 
 # Display the segmentation result
 skin_segmented_image_rgb = cv2.cvtColor(skin_segmented_image, cv2.COLOR_BGR2RGB)
